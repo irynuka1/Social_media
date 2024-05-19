c395ab20534774a408fbdfe4ea7900fb7576290f
alexandrustani <151828997+alexandrustani@users.noreply.github.com>
2024-05-19 15:29:48 +0300

    Update friends.h

--- friends.h
+++ friends.h
@@ -8,7 +8,22 @@
  * Function that handles the input for the friends task
  *
  * @param input - the input string
+ * @param matrix - the matrix of friends
 */
 void handle_input_friends(char *input, int ***matrix);
+
+void add_friend(int ***matrix, char *name);
+
+void remove_friend(int ***matrix, char *name);
+
+void suggestions(int ***matrix, char *name);
+
+void distance(int ***matrix, char *name);
+
+void common(int ***matrix, char *name);
+
+void friends(int ***matrix, char *name);
+
+void popular(int ***matrix, char *name);
 
 #endif // FRIENDS_H
