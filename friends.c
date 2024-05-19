a39e14517b818881d5c300aa84dfee52f31b848f
Sebastian <57030075+irynuka1@users.noreply.github.com>
2024-05-19 14:36:16 +0300

    Update friends.c

--- friends.c
+++ friends.c
@@ -5,35 +5,48 @@
 #include "friends.h"
 #include "users.h"
 
-void handle_input_friends(char *input)
+void add_friend(int ***matrix, char *name)
+{
+	name = strtok(NULL, "\n ");
+	int ind1 = get_user_id(name);
+	name = strtok(NULL, "\n ");
+	int ind2 = get_user_id(name);
+	(*matrix)[ind1][ind2] = 1;
+	(*matrix)[ind2][ind1] = 1;
+	printf("Added connection %s - %s\n", get_user_name(ind1), get_user_name(ind2));
+}
+
+void remove_friend(int ***matrix, char *name)
+{
+	name = strtok(NULL, "\n ");
+	int ind1 = get_user_id(name);
+	name = strtok(NULL, "\n ");
+	int ind2 = get_user_id(name);
+	(*matrix)[ind1][ind2] = 0;
+	(*matrix)[ind2][ind1] = 0;
+	printf("Removed connection %s - %s\n", get_user_name(ind1), get_user_name(ind2));
+}
+
+void handle_input_friends(char *input, int ***matrix)
 {
 	char *commands = strdup(input);
 	char *cmd = strtok(commands, "\n ");
 
 	if (!cmd)
 		return;
-
 	if (!strcmp(cmd, "add"))
-		(void)cmd;
-		// TODO: Add function
+		add_friend(matrix, cmd);
 	else if (!strcmp(cmd, "remove"))
-		(void)cmd;
-		// TODO: Add function
-	else if (!strcmp(cmd, "suggestions"))
-		(void)cmd;
-		// TODO: Add function
-	else if (!strcmp(cmd, "distance"))
-		(void)cmd;
-		// TODO: Add function
-	else if (!strcmp(cmd, "common"))
-		(void)cmd;
-		// TODO: Add function
-	else if (!strcmp(cmd, "friends"))
-		(void)cmd;
-		// TODO: Add function
-	else if (!strcmp(cmd, "popular"))
-		(void)cmd;
-		// TODO: Add function
-
+		remove_friend(matrix, cmd);
+	else if (!strcmp(cmd, "suggestions")) {
+	}
+	else if (!strcmp(cmd, "distance")) {
+	}
+	else if (!strcmp(cmd, "common")) {
+	}
+	else if (!strcmp(cmd, "friends")){
+	}
+	else if (!strcmp(cmd, "popular")) {
+	}
 	free(commands);
 }
