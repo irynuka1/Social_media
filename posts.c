e210c6d3d29744cdf1e7fb3a7dddd03064837220
alexandrustani <151828997+alexandrustani@users.noreply.github.com>
2024-05-21 23:57:35 +0300

    Update posts.c

--- posts.c
+++ posts.c
@@ -4,8 +4,45 @@
 
 #include "users.h"
 #include "posts.h"
+g_node_t *create_post(int *index, char *cmd)
+{
+	cmd = strtok(NULL, " \n");
 
-void handle_input_posts(char *input)
+	int id = get_user_id(cmd);
+
+	char *title = strtok(NULL, "\n");
+
+	g_node_t *root = calloc(1, sizeof(g_node_t));
+
+	root->title = strdup(title);
+
+	root->id = *index;
+
+	root->user_id = id;
+
+	root->children = NULL;
+
+	root->children_count = 0;
+
+	(*index)++;
+
+	printf("Created %s for %s\n", title, get_user_name(id));
+
+	return root;
+}
+
+void add_post(posts_and_reposts_t *social_m, char *cmd, int *index)
+{
+	social_m->pos_and_repos[social_m->pos_count]->root = create_post(index,
+																	 cmd);
+
+	social_m->pos_count++;
+
+	social_m->pos_and_repos = realloc(social_m->pos_and_repos,
+									  social_m->pos_count * sizeof(g_tree_t *));
+}
+
+void handle_input_posts(char *input, posts_and_reposts_t *social_m, int *index)
 {
 	char *commands = strdup(input);
 	char *cmd = strtok(commands, "\n ");
@@ -14,8 +51,7 @@ void handle_input_posts(char *input)
 		return;
 
 	if (!strcmp(cmd, "create"))
-		(void)cmd;
-		// TODO: Add function
+		add_post(social_m, cmd, index);
 	else if (!strcmp(cmd, "repost"))
 		(void)cmd;
 		// TODO: Add function
@@ -43,4 +79,3 @@ void handle_input_posts(char *input)
 
 	free(commands);
 }
-
